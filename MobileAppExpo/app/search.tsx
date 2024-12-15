e586a1af0cec4e5fa21991ce63f32cdbe22d000a
GadeHome <gadlane4ik@gmail.com>
2024-12-15 05:06:57 +0300

    feat: Начал работу

---
+++ MobileAppExpo/app/search.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+
+
+export default function Search() {
+  return (
+    <View
+      style={{
+        flex: 2,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>search</Text>
+    </View>
+  );
+}
+
