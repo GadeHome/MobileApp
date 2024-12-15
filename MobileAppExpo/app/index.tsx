a3484383cc10d963ebddf739378599acc90678c1
GadeHome <gadlane4ik@gmail.com>
2024-12-16 02:21:21 +0300

    feat: эксперементирую с кнопками

--- MobileAppExpo/app/index.tsx
+++ MobileAppExpo/app/index.tsx
@@ -1,23 +1,25 @@
-import { Text, View, StyleSheet } from "react-native";
-import search from "../app/"
+import { Text, View, FlatList, SafeAreaView } from "react-native";
+import { StatusBar } from "expo-status-bar"
+
+import { Button, ButtonText, ButtonSpinner } from "@/components/ui/button"
 
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello world</Text>
+    <View className="flex-[1] bg-black pt-8">
+        <StatusBar style="auto"/>
+        <SafeAreaView />
+        <Button className='p-3'>
+            <ButtonSpinner color="gray-400" />
+            <ButtonText className='font-medium text-sm ml-2'>Please wait...</ButtonText>
+        </Button>
+        <Text className="text-white p-10 text-center">Main menu</Text>
+        <Button className="p-3">
+            <ButtonSpinner color="gray-400" />
+            <ButtonText className='font-medium text-sm ml-2'>It works</ButtonText>
+        </Button>  
+
+        <Text className="text-white justify-center p-3">Hello world</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
