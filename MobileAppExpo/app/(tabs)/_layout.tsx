cec98604cae55917d88cc1d92d5b29b7b6fc3199
GadeHome <gadlane4ik@gmail.com>
2024-12-30 22:38:41 +0300

    style: Подправил цветовую палитру на странице Library

--- MobileAppExpo/app/(tabs)/_layout.tsx
+++ MobileAppExpo/app/(tabs)/_layout.tsx
@@ -12,6 +12,7 @@ export default function TabLayout() {
             options={{
               title: 'Chat',
               tabBarIcon: ({ color }) => <FontAwesome size={26} name="comments" color={color} />,
+              headerShown: false,
             }}
           />
           <Tabs.Screen
@@ -19,6 +20,7 @@ export default function TabLayout() {
             options={{
               title: 'Library',
               tabBarIcon: ({ color }) => <FontAwesome size={26} name="th-list" color={color} />,
+              headerShown: false,
             }}
           />
           <Tabs.Screen
@@ -26,6 +28,7 @@ export default function TabLayout() {
             options={{
               title: 'Home',
               tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+              headerShown: false,
             }}
           />
           <Tabs.Screen
@@ -33,6 +36,7 @@ export default function TabLayout() {
             options={{
               title: 'Search',
               tabBarIcon: ({ color }) => <FontAwesome size={26} name="search" color={color} />,
+              headerShown: false,
             }}
           />
           <Tabs.Screen
@@ -40,6 +44,7 @@ export default function TabLayout() {
             options={{
               title: 'Profile',
               tabBarIcon: ({ color }) => <FontAwesome size={26} name="user-circle-o" color={color} />,
+              headerShown: false,
             }}
           />
         </Tabs>
--- MobileAppExpo/app/(tabs)/library.tsx
+++ MobileAppExpo/app/(tabs)/library.tsx
@@ -1,12 +1,77 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { ScrollView, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import { Button, ButtonText } from '@/components/ui/button';
 
+const books = [
+  {
+    id: 1,
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    cover: 'https://ranobehub.org/api/media/14214/medium',
+  },
+  {
+    id: 2,
+    title: '1984',
+    author: 'George Orwell',
+    cover: 'https://ranobehub.org/api/media/5436/big',
+  },
+  {
+    id: 3,
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    cover: 'https://via.placeholder.com/150',
+  },
 
+  {
+    id: 4,
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    cover: 'https://via.placeholder.com/150',
+
+  }
+];
 
 export default function Library() {
   return (
-    <View className="flex-[1] justify-center">
-      <Text className="text-white text-center text-3xl p-3">library</Text>
+    <View className='flex-[1] bg-[#101010] mt-8'>
+      {/* Search Bar */}
+      <View className='py-4 px-4 bg-[#101010]'>
+        <TextInput
+          className='bg-[#202020] color-white border-0 rounded-lg shadow-sm p-4'
+          placeholder="Search books..."
+          placeholderTextColor="#c0c0c0"
+        />
+      </View>
+
+      {/* Content */}
+      <ScrollView className='flex-[1]'>
+        <View className='px-4'>
+          {/* Book List */}
+          <View className='flex-row flex-wrap justify-between'>
+            {books.map((book) => (
+              <TouchableOpacity
+                key={book.id}
+                className='w-[48%] mb-3 bg-[#202020] rounded-lg shadow-sm p-1.5'
+                onPress={() => {
+                  // Navigate to Book Details Screen
+                  // navigation.navigate('BookDetails', { bookId: book.id });
+                  // or use a modal to display book details
+                  alert(`Title: ${book.title}\nAuthor: ${book.author}`);
+                  // navigation.navigate('BookDetails', { bookId: book.id });
+                  
+                }}
+              >
+                <Image
+                  source={{ uri: book.cover }}
+                  className='w-[100%] h-[200px] object-cover rounded-lg mb-1'
+                />
+                <Text className='text-[14px] text-white'>{book.title}</Text>
+                <Text className='text-[10px] text-[#c0c0c0]'>{book.author}</Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
-
